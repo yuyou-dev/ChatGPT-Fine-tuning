bde90ca6b0c2fc0333fc65ab5c43452709d931e3
余悠 <34769581+yuyou-dev@users.noreply.github.com>
2023-09-14 08:31:04 +0800

    Update preprocess_data.py

--- scripts/preprocess_data.py
+++ scripts/preprocess_data.py
@@ -10,13 +10,12 @@ with open('../data/raw/test_datasets.jsonl', 'r') as file:
 # 随机从6000条数据中取出100条做训练测试
 raw_messages = random.sample(raw_data,100)
 
+# 按OpenAI数据规范，将训练数据按行整理成JSON-L文件
 with open('../data/processed/chat_data.jsonl', 'w') as data_file:
     for item in raw_messages:
-        # 初始化消息对象
         data = {"messages": []}
         data["messages"].append({"role": "system", "content": "你是一个医疗助手"})
         data["messages"].append({"role": "user", "content": item["questions"]})
         data["messages"].append({"role": "assistant", "content": item["answers"]})
-        result = {"messages": data}
-        json.dump(result, data_file, ensure_ascii=False)
+        json.dump({"messages": data}, data_file, ensure_ascii=False)
         data_file.write('\n')
