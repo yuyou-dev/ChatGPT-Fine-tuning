727ed8ecc77f0a9792082fdf7dac05f53fae83ad
tianxue <747012622@qq.com>
2023-11-13 17:05:20 +0800

    删除无关代码

--- scripts/finetuning.py
+++ scripts/finetuning.py
@@ -1,21 +1,10 @@
-import requests
-import json
-
-api_key = ''                                # 从开发者后台获取的api_key
-file_id = ''                                # 从file接口返回的文件id
-
-url = "https://api.openai.com/v1/fine_tuning/jobs"
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": f"Bearer {api_key}"
-}
-data = {
-    "training_file": f"{file_id}",
-    "model": "gpt-3.5-turbo-0613"
-}
-
-response = requests.post(url, headers=headers, json=data)
-print(response.text)                        # 查看响应内容
-response_json = json.loads(response.text)   # 将响应文本转换为 JSON 字典
-fine_tuning_job_id = response_json["id"]    # 获取微调任务id
-print(fine_tuning_job_id)                   # 当前微调模型的id，可用于后续查看模型的状态
+from openai import OpenAI
+import os
+client = OpenAI(
+    api_key=os.environ.get("CUSTOM_ENV_NAME")
+)
+file_tuning_job = client.fine_tuning.jobs.create(
+    training_file='',
+    model="gpt-3.5-turbo"
+)
+print(file_tuning_job)
