65eafa4a1799559515557b553feaf1bb0f8f5094
tianxue <747012622@qq.com>
2023-11-13 15:44:34 +0800

    使用openai的api创建微调任务

--- scripts_1113/finetuning.py
+++ scripts_1113/finetuning.py
@@ -4,7 +4,7 @@ client = OpenAI(
     api_key=os.environ.get("CUSTOM_ENV_NAME")
 )
 file_tuning_job = client.fine_tuning.jobs.create(
-    training_file='file-77ejlGH6kKoZk6kZc2C46A96',
+    training_file='',
     model="gpt-3.5-turbo"
 )
 print(file_tuning_job)
